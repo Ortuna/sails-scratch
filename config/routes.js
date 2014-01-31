43debd647a3a71a1d616515da092c8e709d1aed9
Sumeet Singh <ortuna@gmail.com>
2014-01-31 14:09:20 -0500

    socket.io route

--- config/routes.js
+++ config/routes.js
@@ -34,6 +34,10 @@ module.exports.routes = {
   // (This would also work if you had a file at: `/views/home.ejs`)
   '/': {
     view: 'home/index'
+  },
+  '/items/:message': {
+    controller: 'SocketController',
+    action: 'index'
   }
 
   /*
